5cc3e61d53338e3c9c2c10b85e4e4d57a63819b8
MiguelGalp <85765429+MiguelGalp@users.noreply.github.com>
2021-09-16 13:29:05 -0300

    Update textScramble.js

--- textScramble.js
+++ textScramble.js
@@ -70,7 +70,7 @@ const phrases = [
 'and walking the path'];
 
 
-const el = document.querySelector('[data-block-id="8ea14da2-4227-4559-b318-ea8696146c79"]');
+const el = document.querySelector('[data-block-id="719ee3b8-b250-4a96-8f64-6affbfdcaf09"]');
 const fx = new TextScramble(el);
 
 let counter = 0;
